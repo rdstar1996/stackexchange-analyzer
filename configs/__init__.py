0b1d485572897a4b3df72452377a1600cb650ae3
rdstar1996 <rdstar1996@gmail.com>
2022-06-25 23:59:14 +0530

    added spark config json

---
+++ configs/__init__.py
@@ -0,0 +1,14 @@
+import os
+import json
+import os
+def _readConfigFileData()->dict:
+    jsonData:dict={}
+    configFilePath=os.path.join(os.path.dirname(__file__),"sparkConfigs.json")
+    with open(configFilePath) as fp:
+        jsonData=json.load(fp)
+    return jsonData
+
+def getSparkFromJson()->dict:
+    jsonData=_readConfigFileData()
+    return jsonData["sparkConfigs"]
+
